61e2adc31d392a80125dae762538f969c26bfc49
SalehChowdhury <riphon@gmail.com>
2023-03-12 23:10:24 +0600

    first added files

---
+++ array.js
@@ -0,0 +1,10 @@
+var friendAge = [20,17,18];
+console.log(friendAge[2])
+var sonali = friendAge[1]
+console.log(sonali);
+friendAge[2]= "rchy";           // change "index of 1" position
+console.log(friendAge);
+var position = friendAge.indexOf(17);
+console.log(position)
+var positions = friendAge.indexOf(171);
+console.log(positions)
---
+++ hello.js
@@ -0,0 +1,15 @@
+var friendName = ["eshan","shofi","babu"];
+console.log(friendName);        //result:[ 'eshan', 'shofi', 'babu' ]
+console.log(friendName[0]);     //result: eshan
+console.log(friendName[2]);     //result: babu
+friendName[1]= 20;           // change "index of 1" position
+console.log(friendName);
+var friendAge = [20,17,18];
+var position = friendAge.indexOf(17);    //show index of 17 position
+console.log(position)
+var positions = friendAge.indexOf(171);
+console.log(positions)          //result: -1
+
+
+
+
